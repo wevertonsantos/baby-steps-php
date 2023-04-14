cc0a58bf3706f5b0726e914aa6d40bfe863316fc
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-14 16:02:26 -0300

    operations maths

---
+++ first-lines-code/operacoes.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 20;
+
+$idadeDaqui5Anos = $idade + 5;
+
+// echo $idadeDaqui5Anos;
+
+$soma = 2 + 2;
+$subtrair = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$resto = 2 % 2;
+
+// echo $soma;
