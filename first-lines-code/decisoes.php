6aacbdf2bfdb06c2aaad03e03859d4d842c9b26b
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-14 17:25:12 -0300

    if-elseif-else

--- first-lines-code/decisoes.php
+++ first-lines-code/decisoes.php
@@ -1,13 +1,17 @@
 <?php
 
-$idade = 22;
+$idade = 12;
+$numeroDePessoas = 3;
 
-echo "Você só pode entrar se tiver a partir de 18 anos" . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos ou ";
+echo "se tiver 16 anos e estiver acompanhado" . PHP_EOL;
 
 if ($idade >= 18){
     echo "Você tem $idade anos" . PHP_EOL;
     echo "Pode entrar" . PHP_EOL;
+}else if($idade >= 16 && $numeroDePessoas > 1){
+    echo "Você tem $idade anos e está acompanhado(a), pode entrar!" . PHP_EOL;
 }else{
-    echo "Você tem $idade anos" . PHP_EOL;
-    echo "Não pode entrar" . PHP_EOL;
+    echo "Você tem $idade anos e não está acompanhado(a), ";
+    echo "não pode entrar!" . PHP_EOL;   
 }
