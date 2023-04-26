f5babc3a73883dc9ac6b7b8092a163a9d3980aaa
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-26 16:13:19 -0300

    deposit

--- advanced/bank.php
+++ advanced/bank.php
@@ -4,11 +4,21 @@ function exibeMensagem($mensagem){
 }
 
 function sacarSaldo($conta, $valorASacar){
-    if($valorASacar > $conta){
-        echo "Você não pode sacar" . PHP_EOL;
+    if($valorASacar > $conta['saldo']){
+        exibeMensagem("Você não pode sacar");
     }
     else{
         $conta['saldo'] -= $valorASacar;
+    }
+    return $conta;
+}
+
+function depositarSaldo($conta, $valorADepositar){
+    if($valorADepositar > 0){
+        $conta['saldo'] += $valorADepositar;
+    }
+    else{
+        exibeMensagem("Você somente pode depositar valores positivos");
     }
     return $conta;
 }
@@ -31,6 +41,11 @@ $contaCorrentes = [
 $contaCorrentes['123.456.789-11'] = sacarSaldo(
     $contaCorrentes['123.456.789-11'],
     500
+);
+
+$contaCorrentes['123.456.789-11'] = depositarSaldo(
+    $contaCorrentes['123.456.789-11'],
+    10
 );
 
 foreach ($contaCorrentes as $cpf => $conta) {
