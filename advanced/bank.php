1187c08edaec1bd2a9562e2c554f0888895f97b3
Weverton dos Santos <wever-santos@hotmail.com>
2023-04-27 09:51:50 -0300

    include

--- advanced/bank.php
+++ advanced/bank.php
@@ -1,27 +1,6 @@
 <?php
-function exibeMensagem($mensagem){
-    echo $mensagem . PHP_EOL;
-}
 
-function sacarSaldo( array $conta, float $valorASacar) : array{
-    if($valorASacar > $conta['saldo']){
-        exibeMensagem("Você não pode sacar");
-    }
-    else{
-        $conta['saldo'] -= $valorASacar;
-    }
-    return $conta;
-}
-
-function depositarSaldo(array $conta, float $valorADepositar) : array {
-    if($valorADepositar > 0){
-        $conta['saldo'] += $valorADepositar;
-    }
-    else{
-        exibeMensagem("Você somente pode depositar valores positivos");
-    }
-    return $conta;
-}
+include 'functions.php';
 
 $contaCorrentes = [
     '123.456.789-10' => [
